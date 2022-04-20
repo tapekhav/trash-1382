17fce9da870bce5b0de928051ab61488728c6019
TapekhaVA <tapehavladimir@gmail.com>
2022-04-20 19:43:50 +0300

    itog2

--- solution3.c
+++ solution3.c
@@ -1,77 +1,77 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
+
 typedef struct{
     char *word;
     char *translate;
 }dict;
- 
- 
+
+
 int main() {
     char str1[100][300];
     char str2[100][300];
- 
+
     dict arr[300];
- 
+
     int i = 0;
     int c = 0;
     int f = 0;
- 
+
     for(;;){
         scanf("%s", str1[c]);
         if(!strcmp(str1[c], "ugabuga")) break;
- 
+
         scanf("%s", str2[c]);
- 
+
         for(int y = 0; y < i; ++y){
             if(strcmp(str1[c], arr[y].word) == 0){
                 arr[y].translate = str2[c];
- 
+
                 f = 1;
                 i--;
                 c++;
- 
+
                 break;
             }
- 
+
         }
- 
+
         if(f == 0) {
             arr[i].word = str1[c];
             arr[i].translate = str2[c];
         }
- 
- 
+
+
         f = 0;
         i++;
- 
+
         c++;
     }
-    
+
     getchar();
- 
+
     char str[1000];
     fgets(str, 1000, stdin);
- 
- 
+
+
     char *token;
-    char **array = malloc(1000*sizeof(char*));
- 
+    char *array[1000];
+
     token = strtok(str, " ");
- 
+
     int k = 0;
- 
+
     while(token != NULL){
         array[k] = malloc(100);
         array[k] = token;
- 
+
         token = strtok(NULL, " \n");
         k++;
     }
- 
+
     int flag = 0;
- 
+
     for(int p = 0; p < k; p++){
         for(int n = 0; n < i; n++){
             if(!strcasecmp(array[p], arr[n].word)) {
@@ -83,22 +83,20 @@ int main() {
         if(flag == 0){
             array[p] = "<unknown>";
         }
- 
+
         flag = 0;
     }
- 
+
     int l;
- 
+
     for(l = 0; l < k - 1; ++l){
         printf("%s ", array[l]);
     }
     printf("%s", array[l]);
-    
-   /* for(int j = 0; j < i; ++j){
-        printf("%s---%s\n", arr[j].word, arr[j].translate);
-    } */
-    
-    free(array);
- 
+
+    /* for(int j = 0; j < i; ++j){
+         printf("%s---%s\n", arr[j].word, arr[j].translate);
+     } */
+
     return 0;
 }
